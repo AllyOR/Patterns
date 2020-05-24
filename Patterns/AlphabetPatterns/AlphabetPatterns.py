7d4d5032fdd89cb0e74f092354b9d261a56d47f8
Alison Orellana <ali15.orellan@gmail.com>
2020-05-24 15:55:10 -0400

    letter C

--- Patterns/AlphabetPatterns/AlphabetPatterns.py
+++ Patterns/AlphabetPatterns/AlphabetPatterns.py
@@ -1,6 +1,7 @@
 # Patterns for every letter of the alphabet
 print_A = [ [" " for i in range(6)] for j in range(6) ]
 print_B = [ [" " for i in range(6)] for j in range(6) ]
+print_C = [ [" " for i in range(6)] for j in range(6) ]
 
 # Defining the patterns for the letters
 for raw in range(6):
@@ -12,6 +13,11 @@ for raw in range(6):
     for col in range(6):
         if col == 0 or ((raw==0 or raw==5)and (col!=0 and col!=5)) or (raw==3 and col<5) or (col==5 and (raw==1 or raw==2 or raw==4)):
             print_B[raw][col] = "B"
+
+for raw in range(6):
+    for col in range(6):
+        if ((raw !=0 and raw!=5) and col==0) or ((raw==0 or raw ==5) and col!=0):
+            print_C[raw][col] = "C"
             
 
 # Printing the patterns
@@ -23,5 +29,9 @@ for i in range(6):
     
     for j in range(6):
         print(print_B[i] [j], end = "")
+    print(end="  ")
+    
+    for j in range(6):
+        print(print_C[i] [j], end = "")
         
     print()

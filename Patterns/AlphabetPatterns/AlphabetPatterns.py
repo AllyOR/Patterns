83236a535679a8e2064ac504408601c02f4c3e87
Alison Orellana <ali15.orellan@gmail.com>
2020-05-29 11:06:02 -0400

    pattern H

--- Patterns/AlphabetPatterns/AlphabetPatterns.py
+++ Patterns/AlphabetPatterns/AlphabetPatterns.py
@@ -6,21 +6,22 @@ print_D = [ [" " for i in range(6)] for j in range(6) ]
 print_E = [ [" " for i in range(6)] for j in range(6) ]
 print_F = [ [" " for i in range(6)] for j in range(6) ]
 print_G = [ [" " for i in range(6)] for j in range(6) ]
+print_H = [ [" " for i in range(6)] for j in range(6) ]
 
 # Defining the patterns for the letters
 for raw in range(6):
     for col in range(6):
-        if ((col == 0 or col == 5) and (raw!=0 )) or (raw==0 and (col!=0 and col!=5)) or (raw==3 and (col!=0 and col!=5)):
+        if ((col==0 or col==5) and (raw!=0)) or (raw==0 and (col!=0 and col!=5)) or (raw==3 and (col!=0 and col!=5)):
             print_A[raw][col] = "A"
 
 for raw in range(6):
     for col in range(6):
-        if col == 0 or ((raw==0 or raw==5)and (col!=0 and col!=5)) or (raw==3 and col<5) or (col==5 and (raw==1 or raw==2 or raw==4)):
+        if col==0 or ((raw==0 or raw==5)and (col!=0 and col!=5)) or (raw==3 and col<5) or (col==5 and (raw==1 or raw==2 or raw==4)):
             print_B[raw][col] = "B"
 
 for raw in range(6):
     for col in range(6):
-        if ((raw !=0 and raw!=5) and col==0) or ((raw==0 or raw ==5) and col!=0):
+        if ((raw!=0 and raw!=5) and col==0) or ((raw==0 or raw==5) and col!=0):
             print_C[raw][col] = "C"
 
 for raw in range(6):
@@ -30,18 +31,23 @@ for raw in range(6):
 
 for raw in range(6):
     for col in range(6):
-        if col == 0 or ((raw==0 or raw==5)and (col!=0 and col!=5)) or (raw==3 and col<4):
+        if col==0 or ((raw==0 or raw==5)and (col!=0 and col!=5)) or (raw==3 and col<4):
             print_E[raw][col] = "E"
             
 for raw in range(6):
     for col in range(6):
-        if col == 0 or (raw==0 and (col!=0 and col!=5)) or (raw==3 and (col!=0 and col!=5)):
+        if col==0 or (raw==0 and (col!=0 and col!=5)) or (raw==3 and (col!=0 and col!=5)):
             print_F[raw][col] = "F"
 
 for raw in range(6):
     for col in range(6):
-        if ((raw !=0 and raw!=5) and col==0) or (raw==4 and col==5) or ((raw==0 or raw ==5) and col!=0) or (raw==3 and col>2):
+        if ((raw!=0 and raw!=5) and col==0) or (raw==4 and col==5) or ((raw==0 or raw==5) and col!=0) or (raw==3 and col>2):
             print_G[raw][col] = "G"
+            
+for raw in range(6):
+    for col in range(6):
+        if (col==0 or col==5) or (raw==3 and (col!=0 and col!=5)):
+            print_H[raw][col] = "H"
             
 
 # Printing the patterns
@@ -73,5 +79,9 @@ for i in range(6):
     
     for j in range(6):
         print(print_G[i] [j], end = "")
+    print(end="  ")
+    
+    for j in range(6):
+        print(print_H[i] [j], end = "")
         
     print()

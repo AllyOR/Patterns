41ff41665d61c01d77f753ea77c0f1e4b8abd79f
Alison Orellana <ali15.orellan@gmail.com>
2020-05-31 19:48:35 -0400

    pattern J

--- Patterns/AlphabetPatterns/AlphabetPatterns.py
+++ Patterns/AlphabetPatterns/AlphabetPatterns.py
@@ -8,6 +8,7 @@ print_F = [ [" " for i in range(6)] for j in range(6) ]
 print_G = [ [" " for i in range(6)] for j in range(6) ]
 print_H = [ [" " for i in range(6)] for j in range(6) ]
 print_I = [ [" " for i in range(6)] for j in range(6) ]
+print_J = [ [" " for i in range(6)] for j in range(6) ]
 
 # Defining the patterns for the letters
 for raw in range(6):
@@ -54,6 +55,11 @@ for raw in range(6):
     for col in range(6):
         if col == 3 or ((raw==0 or raw==5)and col!=0):
             print_I[raw][col] = "I"
+
+for raw in range(6):
+    for col in range(6):
+        if col==4 or raw==0 or (raw==5 and col<4) or (col==0 and (raw>2 and raw<5)):
+            print_J[raw][col] = "J"
             
 
 # Printing the patterns
@@ -90,8 +96,12 @@ for i in range(6):
     for j in range(6):
         print(print_H[i] [j], end = "")
     print(end="  ")
-    
+
     for j in range(6):
         print(print_I[i] [j], end = "")
+    print(end="  ")
+    
+    for j in range(6):
+        print(print_J[i] [j], end = "")
         
     print()

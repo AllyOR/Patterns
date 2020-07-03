cc81a63bb2a62309774456dc5d393565d4e449a6
Alison Orellana <ali15.orellan@gmail.com>
2020-07-03 14:28:47 -0400

    pattern 1

--- Patterns/NumberPatterns/NumberPatterns.py
+++ Patterns/NumberPatterns/NumberPatterns.py
@@ -8,8 +8,18 @@ for raw in range(6):
         if ( (raw==0 or raw==5) and (col!=0 and col!=5) ) or ( (col==0 or col==5) and (raw!=0 and raw!=5) ):
             print_0[raw][col] = "0"
 
+for raw in range(6):
+    for col in range(6):
+        if col == 3:
+            print_1[raw][col] = "1"
+
 
 for i in range(6):
+
     for j in range(6):
-        print(print_0[i] [j], end="")
+        print(print_0[i] [j], end = "")
+    print(end="  ")
+    
+    for j in range(6):
+        print(print_1[i] [j], end="")
     print()

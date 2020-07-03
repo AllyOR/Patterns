2cfdfce5ad8d0655f4f93e77a676dd1e6c06ecff
Alison Orellana <ali15.orellan@gmail.com>
2020-07-02 20:16:17 -0400

    pattern 0

--- Patterns/NumberPatterns/NumberPatterns.py
+++ Patterns/NumberPatterns/NumberPatterns.py
@@ -1,2 +1,8 @@
 print_0 = [ [" " for i in range(6)] for j in range(6) ]
 
+for raw in range(6):
+    for col in range(6):
+        if ( (raw==0 or raw==5) and (col!=0 and col!=5) ) or ( (col==0 or col==5) and (raw!=0 and raw!=5) ):
+            print_0[raw][col] = "0"
+
+

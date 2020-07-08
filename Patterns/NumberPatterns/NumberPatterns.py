efc4010701d64827130a60c36757d1d865adef16
Alison Orellana <ali15.orellan@gmail.com>
2020-07-08 08:27:04 -0400

    pattern 2

--- Patterns/NumberPatterns/NumberPatterns.py
+++ Patterns/NumberPatterns/NumberPatterns.py
@@ -2,6 +2,7 @@
 
 print_0 = [ [" " for i in range(6)] for j in range(6) ]
 print_1 = [ [" " for i in range(6)] for j in range(6) ]
+print_2 = [ [" " for i in range(6)] for j in range(6) ]
 
 for raw in range(6):
     for col in range(6):
@@ -13,13 +14,22 @@ for raw in range(6):
         if col == 3 or (raw==5 and (col!=0 and col!=5)) or (raw==2 and col==0) or (raw==1 and col==1) or (raw==0 and col==2):
             print_1[raw][col] = "1"
 
+for raw in range(6):
+    for col in range(6):
+        if ((raw==0 or raw==5)and col!=5) or (col==1 and raw==4) or (col==4 and raw==1) or (col==2 and raw==3) or (col==3 and raw==2) or(col==0 and raw==1):
+            print_2[raw][col] = "2"
+
 
 for i in range(6):
 
     for j in range(6):
         print(print_0[i] [j], end = "")
     print(end="  ")
+
+    for j in range(6):
+        print(print_1[i] [j], end = "")
+    print(end="  ")
     
     for j in range(6):
-        print(print_1[i] [j], end="")
+        print(print_2[i] [j], end="")
     print()

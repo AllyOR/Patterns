7c86097bd544f9f0270067992f7769a84eb0da69
Alison Orellana <ali15.orellan@gmail.com>
2020-07-22 11:38:42 -0400

    pattern 8

--- Patterns/NumberPatterns/NumberPatterns.py
+++ Patterns/NumberPatterns/NumberPatterns.py
@@ -8,6 +8,7 @@ print_4 = [ [" " for i in range(6)] for j in range(6) ]
 print_5 = [ [" " for i in range(6)] for j in range(6) ]
 print_6 = [ [" " for i in range(6)] for j in range(6) ]
 print_7 = [ [" " for i in range(6)] for j in range(6) ]
+print_8 = [ [" " for i in range(6)] for j in range(6) ]
 
 for raw in range(6):
     for col in range(6):
@@ -49,6 +50,11 @@ for raw in range(6):
         if (raw==0) or (col==1 and raw==5) or (col==2 and raw==4) or (col==5 and raw==1) or (col==3 and raw==3) or (col==4 and raw==2):
             print_7[raw][col] = "7"
 
+for raw in range(6):
+    for col in range(6):
+        if (col==0 and (raw==1 or raw==3 or raw==4)) or ((raw==0 or raw==5)and (col!=0 and col!=5)) or (raw==2 and (col!=0 and col!=5)) or (col==5 and (raw==1 or raw==3 or raw==4)):
+            print_8[raw][col] = "8"
+
 
 for i in range(6):
 
@@ -79,7 +85,11 @@ for i in range(6):
     for j in range(6):
         print(print_6[i] [j], end = "")
     print(end="  ")
+
+    for j in range(6):
+        print(print_7[i] [j], end = "")
+    print(end="  ")
     
     for j in range(6):
-        print(print_7[i] [j], end="")
+        print(print_8[i] [j], end="")
     print()
